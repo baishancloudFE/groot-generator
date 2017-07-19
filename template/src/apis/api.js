5bc8f010a55ed4b0b0dbdcc76d80cb0b7d07293c
史宇韬 <yutao.shi@baishancloud.com>
2017-07-19 10:53:23 +0800

    feat：新增测试环境配置

--- template/src/apis/api.js
+++ template/src/apis/api.js
@@ -5,7 +5,7 @@ import { extend } from './extends/'
 export const apis = {
   test: '/static/test.json',
   test2: {
-    group: 'test',
+    group: 'example',
     url: '/static/test.json',
     extend: extend.test2
   }
