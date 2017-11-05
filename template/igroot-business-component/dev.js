9295ce8ada91ca039b2da10b5e884140e54b1649
史宇韬 <yutao.shi@baishancloud.com>
2017-11-05 18:39:35 +0800

    业务组件模板调整

--- template/igroot-business-component/dev.js
+++ template/igroot-business-component/dev.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Component from './index'
+import Component from './src/demo'
 
 ReactDOM.render(
   React.createElement(Component),
